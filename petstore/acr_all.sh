6f1a244029c0c63f42208c5ef6ca31365b892fe6
Sergii Tychkin <sergiy.tychkin@gmail.com>
2024-10-20 00:15:39 +0300

    TSV: refactoring before start new

--- petstore/acr_all.sh
+++ petstore/acr_all.sh
@@ -1,5 +1,5 @@
 # Build images directly to azure container registry
-# Usage: ./bin/acr_all.sh
+# Usage: ./acr_all.sh
 function acr_build() {
 imagename=$1
 tag=$2
@@ -10,7 +10,7 @@ dockerfile="Dockerfile"
 az acr build \
 --image $imagename:$tag \
 --registry $acrname \
---file $path$dockerfile \
+--file $path/$dockerfile \
 $path
 }
 # Login to Azure
@@ -20,16 +20,14 @@ PETSTOREAPP="petstoreapp"
 PETSTOREORDERSERVICE="petstoreorderservice"
 PETSTOREPETSERVICE="petstorepetservice"
 PETSTOREPRODUCTSERVICE="petstoreproductservice"
-ORDERITEMSRESERVER="orderitemsreserver"
 
 IMAGE_TAG="latest"
 ACR_NAME="22tsvacr"
-PATH_TO_DOCKERFILE="../"
+PATH_TO_DOCKERFILE="../petstore/"
 
 # Build the images
 acr_build $PETSTOREAPP $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREAPP
-# acr_build $PETSTOREORDERSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREORDERSERVICE
-# acr_build $PETSTOREPETSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREPETSERVICE
-# acr_build $PETSTOREPRODUCTSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREPRODUCTSERVICE
-# acr_build $ORDERITEMSRESERVER $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$ORDERITEMSRESERVER
+acr_build $PETSTOREORDERSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREORDERSERVICE
+acr_build $PETSTOREPETSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREPETSERVICE
+acr_build $PETSTOREPRODUCTSERVICE $IMAGE_TAG $ACR_NAME $PATH_TO_DOCKERFILE$PETSTOREPRODUCTSERVICE
 # End of file
